54a7a8e68e7a84a56dd033bb0e0eff2ebf64b9f5
Luca Borda <luca@northic.io>
2021-01-07 02:12:39 -0300

    hover exception added

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -9,13 +9,13 @@ export default class Row extends React.Component {
         var moreNumb = ''
         var displaySpan = 'none'
         var displayClip = 'none'
-        var weightFrom = '400'
-        var weightDate = '400'
+        var notHoverFrom = ''
+        var notHoverDate = ''
 
         if (columnStyle === "formColumn"){
-            weightFrom = '600'
+            notHoverFrom = 'notHover'
         } else if (columnStyle === "dateColumn"){
-            weightDate = '600'
+            notHoverDate = 'notHover'
         }
 
         if (adj === true){
@@ -29,8 +29,8 @@ export default class Row extends React.Component {
         }
 
         return(
-            <div key={id} className="rows">
-                <div style={{fontWeight: (weightFrom)}} className="formColumn" >{from}</div>
+            <div style={{}} key={id} className="rows">
+                <div className={notHoverFrom} >{from}</div>
                 <div>
                     <div className="showedMail">{to}</div>
                     <span style={{display: (displaySpan)}} className="moreNumber">{moreNumb}</span>
@@ -39,7 +39,7 @@ export default class Row extends React.Component {
                     <div className="showedMail">{subject}</div>
                     <img src={clipImg} style={{display: (displayClip)}} className="clipImg"/>
                 </div>
-                <div style={{fontWeight: (weightDate)}} className="dateColumn" >{date}</div>
+                <div className={notHoverDate} >{date}</div>
             </div>
         )
     }
